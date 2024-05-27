d700d744dc83a64b76957834ca8ddaf91698251d
Grld-Andy <andyansong82@gmail.com>
2024-05-27 01:49:43 +0100

    fix: imports for context

--- src/components/WindowLayout/WindowLayout.tsx
+++ src/components/WindowLayout/WindowLayout.tsx
@@ -3,7 +3,7 @@ import Sidebar from '../Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
 import MusicPlayer from '../MusicPlayer/MusicPlayer'
 import './style.css'
-import ThemeContext from '../../contexts/ThemeContext'
+import {ThemeContext} from '../../contexts/ThemeContext'
 
 const WindowLayout: React.FC = () => {
   const {theme} = useContext(ThemeContext)
--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,9 +1,20 @@
-import { createContext, Dispatch, useEffect, useReducer } from "react";
+import { createContext, Dispatch, ReactNode, useEffect, useReducer } from "react";
 import { themeReducer } from "../reducers/ThemeReducer";
 
-export const ThemeContext = createContext()
+interface ThemeContextType {
+    theme: string
+    dispatch: Dispatch<{ type: string }>
+}
+interface Props {
+    children: ReactNode
+}
 
-const ThemeContextProvider = (props) => {
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: 'dark',
+    dispatch: () => null
+})
+
+const ThemeContextProvider: React.FC<Props> = (props) => {
     const [theme, dispatch]: [string, Dispatch<{ type: string; }>] = useReducer(themeReducer, 'dark', () => {
         const localTheme: string|null = localStorage.getItem('theme')
         return localTheme ? localTheme : 'dark'
