417128b6f4e2b0250aef00986bc88ea82fb69d47
Grld-Andy <andyansong82@gmail.com>
2024-06-04 22:08:35 +0100

    fix: show lyrics

--- src/components/WindowLayout/WindowLayout.tsx
+++ src/components/WindowLayout/WindowLayout.tsx
@@ -6,15 +6,18 @@ import './style.css'
 import {ThemeContext} from '../../contexts/ThemeContext'
 import ContextMenu from '../ContextMenu/ContextMenu'
 import LyricsView from '../LyricsView/LyricsView'
+import { CurrentSongContext } from '../../contexts/CurrentSongContext'
 
 const WindowLayout: React.FC = () => {
   // theme
   const {theme} = useContext(ThemeContext)
+  const {currentSong} = useContext(CurrentSongContext)
 
   // handle lyrics display
   const [showLyrics, setShowLyrics] = useState<boolean>(false)
   const displayLyrics = () => {
-    setShowLyrics(!showLyrics)
+    if(currentSong.song)
+      setShowLyrics(!showLyrics)
   }
   
 
